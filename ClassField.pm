0f8acab5a8eff372976dfc3464fc6bf7034de2cd
Olaf Seibert <rhialto@falu.nl>
2016-04-27 19:09:44 +0200

    Use code blocks to fill function bodies of generated functions.

--- ClassField.pm
+++ ClassField.pm
@@ -154,8 +154,9 @@ sub analyze_function_pointer
 	    # Definition: FooFunc %s(int i, long l)
 	    $pat = funcTypedef2definition($type, $self->{declaration_pattern});
 	    my $define = sprintf $pat, $value;
+            my $body = $widget->{code_blocks}->{$value}->{body} || "";
 
-	    $for_class->{define_class_functions} .= "${define}\n{\n}\n\n";
+	    $for_class->{define_class_functions} .= "${define}\n{\n${body}\n}\n\n";
 	}
     }
 }
