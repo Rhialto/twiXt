b28523725d1bd511698370d56795ba5f0f53c4a0
Olaf Seibert <rhialto@falu.nl>
2016-10-27 20:18:44 +0200

    Add example widget (Joystick) and test program.

---
+++ joystick/main.c
@@ -0,0 +1,233 @@
+/*
+ * Some sample code to start up an application using something other
+ * than the default visual, and to test some widget by placing it in
+ * a toplevel window.
+ *
+ * To compile:
+ *	cc -g visual.c -o visual -lXaw -lXmu -lXt -lXext -lX11 -lm
+ *
+ * To run:
+ *	./visual -geometry 300x300 -depth 24 -visual StaticColor -fg blue -bg yellow
+ *
+ * you need to move the mouse to get the particular visuals colormap
+ * to install.
+ */
+
+#include <stdio.h>
+
+#include <X11/Intrinsic.h>
+#include <X11/StringDefs.h>
+#include <X11/Shell.h>
+#include <X11/Xaw/Form.h>
+
+#include "Joystick.h"
+
+typedef struct
+{
+	Visual	*visual;
+	int	depth;
+} OptionsRec;
+
+OptionsRec	Options;
+
+XtResource resources[] =
+{
+    {"visual", "Visual", XtRVisual, sizeof (Visual *),
+	XtOffsetOf (OptionsRec, visual), XtRImmediate, NULL},
+    {"depth", "Depth", XtRInt, sizeof (int),
+	XtOffsetOf (OptionsRec, depth), XtRImmediate, NULL},
+};
+
+XrmOptionDescRec Desc[] =
+{
+    {"-visual", "*visual", XrmoptionSepArg, NULL},
+    {"-depth", "*depth", XrmoptionSepArg, NULL}
+};
+
+
+struct TimerCtx {
+    XtAppContext app_context;
+    Widget joy1, joy2;
+    int counter;
+    Pixel blue, pink;
+};
+
+struct TimerCtx ctx;
+
+/*
+ * Every so often this function gets called.
+ *
+ * This makes the program somewhat active when the tested widgets
+ * don't do that by themselves.
+ */
+void TimerCallbackProc(XtPointer client_data, XtIntervalId *timer)
+{
+    struct TimerCtx    *ctx = (struct TimerCtx *)client_data;
+    Arg			args[2];
+    int			cnt;
+
+    //fprintf(stderr, "TimerCallbackProc\n");
+
+    cnt = 0;
+
+    XtSetArg(args[cnt], XtNenableBits, ctx->counter); ++cnt;
+    XtSetValues(ctx->joy1, args, cnt);
+
+    /* quickly modify the value in-place */
+    args[cnt-1].value = ~ctx->counter;
+
+    if ((ctx->counter % 10) == 0) {
+	Pixel color = (ctx->counter % 20) ? ctx->pink : ctx->blue;
+
+	XtSetArg(args[cnt], XtNdirectionColor, color); cnt++;
+    }
+
+    XtSetValues(ctx->joy2, args, cnt);
+
+    ctx->counter++;
+
+    /* Re-arm the timer */
+    XtAppAddTimeOut(ctx->app_context, 666, TimerCallbackProc, client_data);
+}
+
+
+
+
+int
+main (int argc, char **argv)
+{
+    XtAppContext	app;		/* the application context */
+    Widget		top;		/* toplevel widget */
+    Display	       *dpy;		/* display */
+    char	      **xargv;		/* saved argument vector */
+    int			xargc;		/* saved argument count */
+    Colormap		colormap;	/* created colormap */
+    XVisualInfo		vinfo;		/* template for find visual */
+    XVisualInfo	       *vinfo_list;	/* returned list of visuals */
+    int			count;		/* number of matchs (only 1?) */
+    Arg			args[10];
+    Cardinal		cnt;
+    char	       *name = "test";
+    char	       *class = "Test";
+    Widget		form;
+    Widget		joy1, joy2;
+    XColor		screen_def, exact_def;
+
+    /*
+     * save the command line arguments
+     */
+
+    xargc = argc;
+    xargv = (char **) XtMalloc (argc * sizeof (char *));
+    bcopy ((char *) argv, (char *) xargv, argc * sizeof (char *));
+
+    /*
+     * The following creates a _dummy_ toplevel widget so we can
+     * retrieve the appropriate visual resource.
+     */
+    cnt = 0;
+    top = XtOpenApplication(&app, class, Desc, XtNumber(Desc), &argc, argv,
+			    (String *)NULL, applicationShellWidgetClass,
+			    args, cnt);
+    dpy = XtDisplay (top);
+    cnt = 0;
+    XtGetApplicationResources(top, &Options, resources,
+			      XtNumber (resources),
+			      args, cnt);
+    cnt = 0;
+    if (Options.visual && Options.visual != DefaultVisualOfScreen(XtScreen(top))) {
+	XtSetArg(args[cnt], XtNvisual, Options.visual); ++cnt;
+	/*
+	 * Now we create an appropriate colormap.  We could
+	 * use a default colormap based on the class of the
+	 * visual; we could examine some property on the
+	 * rootwindow to find the right colormap; we could
+	 * do all sorts of things...
+	 */
+	colormap = XCreateColormap(dpy,
+				   RootWindowOfScreen(XtScreen(top)),
+				   Options.visual,
+				   AllocNone);
+	XtSetArg(args[cnt], XtNcolormap, colormap); ++cnt;
+
+	/*
+	 * Now find some information about the visual.
+	 */
+	vinfo.visualid = XVisualIDFromVisual(Options.visual);
+	vinfo_list = XGetVisualInfo(dpy, VisualIDMask, &vinfo, &count);
+
+	if (vinfo_list && count > 0) {
+	    XtSetArg(args[cnt], XtNdepth, vinfo_list[0].depth);
+	    ++cnt;
+	    XFree((XPointer) vinfo_list);
+	}
+    } else {
+	colormap = DefaultColormapOfScreen(XtScreen(top));
+    }
+    XtDestroyWidget (top);
+
+    /*
+     * Now create the real toplevel widget.
+     */
+    XtSetArg(args[cnt], XtNargv, xargv); ++cnt;
+    XtSetArg(args[cnt], XtNargc, xargc); ++cnt;
+
+    top = XtAppCreateShell((char *) NULL, class,
+			   applicationShellWidgetClass,
+			   dpy, args, cnt);
+
+    /*
+     * Add a Form into the toplevel, so we can put
+     * some of the widgets into it that we really wanted to test.
+     */
+    form = XtVaCreateManagedWidget("form",
+				   formWidgetClass, top,
+				   NULL);
+
+    joy1 = XtVaCreateManagedWidget("joystick1",
+				   joystickWidgetClass, form,
+				   XtNwidth, 32,
+				   XtNheight, 32,
+				   /* Constraints */
+				   NULL);
+
+    XAllocNamedColor(dpy, colormap, "yellow",
+		     &screen_def, &exact_def);
+
+    joy2 = XtVaCreateManagedWidget("joystick2",
+				   joystickWidgetClass, form,
+				   XtNwidth, 64,
+				   XtNheight, 64,
+				   XtNenableBits, 31,
+				   XtNbackground,  screen_def.pixel,
+				   XtNborderWidth, 0,
+				   /* Constraints */
+				   XtNfromHoriz, joy1,
+				   NULL);
+    /*
+     * Allocate some colours for changing the widget appearance.
+     */
+    XAllocNamedColor(dpy, colormap, "blue",
+		     &screen_def, &exact_def);
+    ctx.blue = screen_def.pixel;
+
+    XAllocNamedColor(dpy, colormap, "hotpink",
+		     &screen_def, &exact_def);
+    ctx.pink = screen_def.pixel;
+
+    ctx.joy1 = joy1;
+    ctx.joy2 = joy2;
+    ctx.counter = 0;
+    ctx.app_context = app;
+
+    XtAppAddTimeOut(app, 666, TimerCallbackProc, &ctx);
+
+    /*
+     * Display the application and loop handling all events.
+     */
+    XtRealizeWidget(top);
+    XtAppMainLoop(app);
+
+    return 0;
+}
+
